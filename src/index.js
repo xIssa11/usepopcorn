c91ab917e1e4825b5fa03ae0a835420b897ee3fe
xIssa11 <issahashim@yahoo.com>
2024-03-20 19:54:26 -0700

    Star Rating Component Completed

--- src/index.js
+++ src/index.js
@@ -8,5 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={10} />
+    <StarRating
+      size={24}
+      color="red"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      maxRating={5}
+    />
   </React.StrictMode>
 );
